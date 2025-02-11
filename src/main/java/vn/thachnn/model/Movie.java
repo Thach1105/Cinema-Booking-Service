ef19bc523be2b68032e8a95c539ffb6493517c1c
thachnn <thach11052002@gmail.com>
2025-02-11 22:58:16 +0700

    add Movie Entity

---
+++ src/main/java/vn/thachnn/model/Movie.java
@@ -0,0 +1,42 @@
+package vn.thachnn.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import vn.thachnn.common.MovieStatus;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title", length = 512, nullable = false)
+    private String title;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "duration")
+    private Integer duration;
+
+    @Column(name = "age_limit")
+    private Integer ageLimit;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private MovieStatus status;
+}
