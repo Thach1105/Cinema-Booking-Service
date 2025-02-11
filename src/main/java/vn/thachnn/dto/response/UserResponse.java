81f8352074e86bb3ce7f84bdcdff6d882532dd29
thachnn <thach11052002@gmail.com>
2025-02-11 22:44:58 +0700

    add UserResponse DTO for returning user details

---
+++ src/main/java/vn/thachnn/dto/response/UserResponse.java
@@ -0,0 +1,28 @@
+package vn.thachnn.dto.response;
+
+import lombok.*;
+import vn.thachnn.common.Gender;
+import vn.thachnn.common.UserType;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse implements Serializable {
+
+    private Long id;
+    private String username;
+    private String fullName;
+    private LocalDate dateOfBirth;
+    private Gender gender;
+    private String phone;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private UserType type;
+}
