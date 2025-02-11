427c64dc331a7273e7039fbdf114dc2a6bd637bf
thachnn <thach11052002@gmail.com>
2025-02-11 22:41:59 +0700

    add UserPasswordRequest DTO for password update

---
+++ src/main/java/vn/thachnn/dto/request/UserPasswordRequest.java
@@ -0,0 +1,18 @@
+package vn.thachnn.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class UserPasswordRequest {
+
+    @NotNull(message = "id must be not null")
+    private Long id;
+
+    @NotBlank(message = "password must be not blank")
+    private String password;
+
+    @NotBlank(message = "confirmPassword be not blank")
+    protected String confirmPassword;
+}
