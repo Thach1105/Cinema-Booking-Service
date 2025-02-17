09bbcf89293fd5bdbef0bfa1f1711f45a5bf0fab
thachnn <thach11052002@gmail.com>
2025-02-17 10:50:55 +0700

    Create TokenResponse DTO for handling access and refresh tokens

---
+++ src/main/java/vn/thachnn/dto/response/TokenResponse.java
@@ -0,0 +1,14 @@
+package vn.thachnn.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class TokenResponse {
+
+    private String accessToken;
+    private String refreshToken;
+}
