cbe24729589e39a02982caddb1d1433b3bcfccc6
thachnn <thach11052002@gmail.com>
2025-02-17 10:50:37 +0700

    Create SignInRequest DTO for user sign-in with validation

---
+++ src/main/java/vn/thachnn/dto/request/SignInRequest.java
@@ -0,0 +1,16 @@
+package vn.thachnn.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SignInRequest implements Serializable {
+
+    @NotBlank(message = "username must be not blank")
+    private String username;
+
+    @NotBlank(message = "password must be not blank")
+    private String password;
+}
