cf6e1dbb19f9bf92448352971aad3841fc74384d
thachnn <thach11052002@gmail.com>
2025-02-17 10:49:28 +0700

    Define JwtService interface for token generation and validation

---
+++ src/main/java/vn/thachnn/service/JwtService.java
@@ -0,0 +1,18 @@
+package vn.thachnn.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import vn.thachnn.common.TokenType;
+
+import java.util.Collection;
+
+public interface JwtService {
+
+    String generateAccessToken (long userId, String username, Collection<? extends GrantedAuthority> authorities);
+
+    String generateRefreshToken(long userId, String username, Collection<? extends GrantedAuthority> authorities);
+
+    String extractUsername(String token, TokenType type);
+
+    boolean isTokenValid(String token, TokenType type, UserDetails userDetails);
+}
