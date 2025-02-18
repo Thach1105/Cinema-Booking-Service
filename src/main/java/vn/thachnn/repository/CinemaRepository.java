0f7aa810b1980c0b21dde330231e172608751dcf
thachnn <thach11052002@gmail.com>
2025-02-18 09:09:46 +0700

    Add CinemaRepository

---
+++ src/main/java/vn/thachnn/repository/CinemaRepository.java
@@ -0,0 +1,15 @@
+package vn.thachnn.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import vn.thachnn.model.Cinema;
+
+@Repository
+public interface CinemaRepository extends JpaRepository<Cinema, Long> {
+
+    @Query(value = "SELECT c FROM Cinema c WHERE c.city LIKE :city")
+    Page<Cinema> searchByCity (String city, Pageable pageable);
+}
