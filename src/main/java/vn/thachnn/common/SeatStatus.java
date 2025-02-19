a6b9718ec8914d49aeef68b4acb8895938ab5aab
thachnn <thach11052002@gmail.com>
2025-02-19 18:34:08 +0700

    Add SeatStatus enum to manage seat statuses

---
+++ src/main/java/vn/thachnn/common/SeatStatus.java
@@ -0,0 +1,10 @@
+package vn.thachnn.common;
+
+public enum SeatStatus {
+
+    AVAILABLE,  /*có sẵn*/
+    RESERVED,   /*đã đặt*/
+  /*  BLOCK,      *//*không được sử dụng*/
+    PENDING     /*đang được tạm giữ*/
+    ;
+}
